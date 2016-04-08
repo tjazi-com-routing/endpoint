69a8d6e2c643f3ba3dbce66083389afe2cc72157
Krzysztof Wasiak <krzysztof.wasiak@wipro.com>
2016-04-08 10:16:49 +0100

    Correcting pom.xml

--- template/src/main/java/com/tjazi/routing/endpoint/template/EndpointTemplateCoreApplication.java
+++ template/src/main/java/com/tjazi/routing/endpoint/template/EndpointTemplateCoreApplication.java
@@ -2,8 +2,10 @@ package com.tjazi.routing.endpoint.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.tjazi")
 public class EndpointTemplateCoreApplication {
 
 	public static void main(String[] args) {
